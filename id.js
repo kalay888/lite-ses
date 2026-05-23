2d20a9f88b1ea60eee2bd22a404f192d3d9ed2dc
kalay888 <upthirisackmahamouth@gmail.com>
2026-05-23 07:10:51 -0700

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,37 @@
-function malvinid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 2; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
-  }
-  return result;
-}
-module.exports = {malvinid};
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+
+    res.send(`
+    <html>
+
+    <head>
+      <title>ZACK-MD PAIR CODE</title>
+    </head>
+
+    <body style="
+    background:#111;
+    color:white;
+    font-family:sans-serif;
+    text-align:center;
+    padding-top:80px;
+    ">
+
+    <img src="https://files.catbox.moe/xd30uv.png" width="220"/>
+
+    <h1>ZACK-MD PAIR CODE</h1>
+
+    <p>PAIR CODE SERVER CONNECTED ✅</p>
+
+    </body>
+    </html>
+    `)
+
+});
+
+app.listen(PORT, () => {
+   console.log("Server Running");
+});
